fc0f8dcdfb987fd000027cda0171e67637a59738
Daniel Rojas <daniel.drojas@outlook.com>
2020-08-03 21:01:03 -0400

    Updates

---
+++ precache-manifest.0e2eae7529f6d07dbb9e8693269266c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e09a80e8841669e3f6f03b0ea3bb93",
+    "url": "/employee-repository/index.html"
+  },
+  {
+    "revision": "ca74df23767bed3bdd30",
+    "url": "/employee-repository/static/js/2.a900e40c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-repository/static/js/2.a900e40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a55d2e3584a54de96b",
+    "url": "/employee-repository/static/js/main.f6f13cec.chunk.js"
+  },
+  {
+    "revision": "ddfc9fd4cb8e02feca4c",
+    "url": "/employee-repository/static/js/runtime-main.876553d3.js"
+  }
+]);
